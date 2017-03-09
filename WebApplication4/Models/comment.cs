35a808eafde156356216504ce98c9d13d976e9ac
nareksahakyan <nareksahakyan@gmx.com>
2017-03-09 21:25:36 +0400

    blabla

---
+++ WebApplication4/Models/comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class comment
+    {
+        public int id { get; set; }
+        public string comText { get; set; }
+    }
+}
